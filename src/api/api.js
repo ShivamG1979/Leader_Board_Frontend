a9eed2695ebb0f73151635445c0aeafefb93647b
ShivamG1979 <shivamg1504@gmail.com>
2024-10-17 15:59:07 +0530

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://leader-board-backend-1.onrender.com/api' });
 
 export const fetchUsers = () => API.get('/users');
 export const addUser = (name) => API.post('/users', { name });
